79b502ee154bba9175904630a7f8e22c5c9795a9
n37le <anhminh05102004@gmail.com>
2024-01-15 14:00:32 -0500

    Modify title, heading and create a sketch box

---
+++ script.js
@@ -0,0 +1,12 @@
+
+for(let i = 0; i < 256;i++){
+    let div = document.createElement('div');
+    div.style.height = "31.25px";
+    div.style.minWidth = "31.25px";
+    div.style.backgroundColor = "lightgray";
+    div.style.border = "0px";
+    div.style.padding = "0px";
+    div.style.margin = "0px";
+    div.style.display = "flex";
+    document.querySelector('.container').appendChild(div);
+}
